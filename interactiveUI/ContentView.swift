31b80e6cc7317b76383a5061f68d2d36b3790097
Evie <Samantha.Fisher123@gmail.com>
2025-06-06 09:45:24 -0500

    change button name, made code react to user imput.

--- interactiveUI/ContentView.swift
+++ interactiveUI/ContentView.swift
@@ -8,21 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var name = ""
+  @State private var textTitle = "What is your name?"
+    
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
-            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-        .multilineTextAlignment(.center)
-        .font(.title)
-        .border(Color.gray, width: 1)
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            TextField("Type your name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submite name") {
+                textTitle = "welcome \(name)!"
             }
             //end button
             .font(.title)
             .buttonStyle(.borderedProminent)
-            .tint(.cyan)
+            .tint(.teal.opacity(0.9))
         }
         //end VStack
         .padding()
